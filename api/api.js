b257345c3a0fc3032d94218a88e76ded514cf99f
deolsatish <deol.satish.2001.3@gmail.com>
2020-07-31 08:41:51 +1000

    I should have done this before now. I am sorry.

---
+++ api/api.js
@@ -0,0 +1,354 @@
+const mongoose = require('mongoose');
+//mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true });
+mongoose.connect('mongodb+srv://deolsatish:debarati@sit209.udjho.mongodb.net/test');
+
+
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+const port = process.env.PORT || 5000;
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+/**
+* @api {get} /api/test tests if api is working
+* @apiGroup Test
+* @apiSuccessExample {String} Success-Response:
+*"The API is working!"
+*/
+
+
+
+app.get('/api/test', (req, res) => {
+res.send('The API is working!');
+});
+app.listen(port, () => {
+console.log(`listening on port ${port}`);
+});
+const Device = require('./models/device');
+const User = require('./models/user');
+
+
+
+
+
+/**
+* @api {get} /api/devices AllDevices An array of all devices
+* @apiGroup Device
+* @apiSuccessExample {json} Success-Response:
+* [
+*{
+*"_id": "dsohsdohsdofhsofhosfhsofh",
+*"name": "Mary's iPhone",
+*"user": "mary",
+*"sensorData": [
+*{
+*"ts": "1529542230",
+*"temp": 12,
+*"loc": {
+*"lat": -37.84674,
+*"lon": 145.115113
+*}
+*},
+*{
+*"ts": "1529572230",
+*"temp": 17,
+*"loc": {
+*"lat": -37.850026,
+*"lon": 145.117683
+*}
+*}
+*]
+*}
+* ]
+* @apiErrorExample {json} Error-Response:
+* {
+*
+"User does not exist"
+* }
+*/
+
+app.get('/api/devices', (req, res) => {
+    Device.find({}, (err, devices) => {
+    if (err == true) {
+    return res.send(err);
+    } else {
+    return res.send(devices);
+    }
+    });
+});
+/**
+* @api {post} /api/devices Adds new device
+* @apiGroup Device
+* @apiParam {json}:
+* [
+*    {
+*        "name": "Mary's iPhone",
+*        "user": "mary",
+*        "sensorData": [
+*            {
+*                "ts": "1529542230",
+*                "temp": 12,
+*                "loc": 
+*                {
+*                    "lat": -37.84674,
+*                   "lon": 145.115113
+*                }
+*            },
+*            {
+*               "ts": "1529572230",
+*                "temp": 17,
+*                "loc": 
+*                {
+*                    "lat": -37.850026,
+*                    "lon": 145.117683
+*               }
+*           }
+*        ]
+*    }
+*] 
+* @apiSuccessExample {String} Success-Response: 
+*"successfully added device and data'" 
+* @apiErrorExample {json} Error-Response:
+{
+    "User does not exist"
+}
+*/
+
+
+app.post('/api/devices', (req, res) => {
+    const { name, user, sensorData } = req.body;
+    const newDevice = new Device({
+    name,
+    user,
+    sensorData
+    });
+    newDevice.save(err => {
+    return err
+    ? res.send(err)
+    : res.send('successfully added device and data');
+    });
+});
+/**
+* @api {post} /api/authenticate Verifies User
+* @apiGroup Users
+* @apiParam {json}:
+* {
+*    "name": "Mary's iPhone",
+*    "password": test123"
+*        
+*}
+* @apiSuccessExample {json} Success-Response:{
+*                    success: true,
+*                    message: 'Authenticated successfully',
+*                    isAdmin: result.isAdmin} 
+* @apiErrorExample {String} Error-Response:
+*Error:(User doesn't exist)The User in not in the Registration Database
+*/
+
+
+
+
+app.post('/api/authenticate', (req, res) => {
+    const { name, password} = req.body;
+    console.log("suthenticate name:"+name);
+    console.log("authenticate password:"+password);
+    
+    User.findOne({"name":name}, (err, result) => {
+        if(err)
+        return err;
+        console.log("Result");
+        console.log(result);
+        if(result==null)
+        {
+            res.send("Error:(User doesn't exist)The User in not in the Registration Database");
+        }
+        else
+        {
+            if(result.password==password)
+            {
+                console.log("password else");
+                return res.json({
+                    success: true,
+                    message: 'Authenticated successfully',
+                    isAdmin: result.isAdmin}
+                );                
+            }
+            else
+            {
+                res.send("Error: Password is incorrect");
+            }
+        }
+    });
+
+    
+});
+/**
+* @api {post} /api/registration Adds new Users
+* @apiGroup Users
+* @apiParam {json}:
+* {
+*    "name": "Mary's iPhone",
+*    "password": test123",
+*    "isAdmin":0
+*        
+*}
+* @apiSuccessExample {String} Success-Response:
+*"Created new user" 
+* @apiErrorExample {String} Error-Response:
+*"Error!!! User already exists"
+*/
+
+app.post('/api/registration', (req, res) => {
+    const { name, password, isAdmin } = req.body;
+    User.find({}, (err, users) => {
+        console.log("users");
+        console.log(users);
+    });
+    User.findOne({"name":name}, (err, result) => {
+        if(err)
+        return err;
+        console.log("Result");
+        console.log(result);
+        if(result!=null)
+        {
+            res.send("Error!!! User already exists");
+        }
+        else
+        {
+            const newUser = new User({
+                name: name,
+                password,
+                isAdmin
+            });
+            newUser.save(err => {
+                return err
+                ? res.send(err)
+                : res.json({
+                success: true,
+                message: 'Created new user'
+                });
+            });
+        }
+    });
+    
+});
+
+
+/**
+* @api {get} /api/devices/:deviceId/device-history Returns Device History for specific device id
+* @apiGroup Device
+* @apiParam {json}:
+* {
+*    "_id": "dsohsdohsdofhsofhosfhsofh"
+*}
+* @apiSuccessExample {json} Success-Response:
+*"sensorData": [
+*            {
+*                "ts": "1529542230",
+*                "temp": 12,
+*                "loc": 
+*                {
+*                    "lat": -37.84674,
+*                    "lon": 145.115113
+*                }
+*            },
+*            {
+*                "ts": "1529572230",
+*                "temp": 17,
+*                "loc": 
+*                {
+*                    "lat": -37.850026,
+*                    "lon": 145.117683
+*                }
+*    }
+*] 
+* @apiErrorExample {json} Error-Response: 
+{
+    Device Does not Exist
+}
+*/
+
+
+
+app.get('/api/devices/:deviceId/device-history', (req, res) => {
+    const { deviceId } = req.params;
+    Device.findOne({"_id": deviceId }, (err, devices) => {
+    const { sensorData } = devices;
+    return err
+    ? res.send(err)
+    : res.send(sensorData);
+    });
+    });
+
+
+app.post('/api/send-command', (req, res) => {
+    console.log(req.body);
+});
+/**
+* @api {get} /api/users/:user/devices Returns device for specific user
+* @apiGroup User
+* @apiSuccessExample {String} Success-Response:
+* [
+*    {
+*        "_id": "dsohsdohsdofhsofhosfhsofh",
+*        "name": "Mary's iPhone",
+*        "user": "mary",
+*        "sensorData": [
+*            {
+*                "ts": "1529542230",
+*                "temp": 12,
+*                "loc": 
+*                {
+*                    "lat": -37.84674,
+*                    "lon": 145.115113
+*                }
+*            },
+*            {
+*                "ts": "1529572230",
+*                "temp": 17,
+*                "loc": 
+*                {
+*                    "lat": -37.850026,
+*                    "lon": 145.117683
+*                }
+*            }
+*        ]
+*    }
+*] 
+* @apiErrorExample {json} Error-Response:
+{
+    "Device does not exist"
+}
+*/
+
+app.get('/api/users/:user/devices', (req, res) => {
+    const { user } = req.params;
+    Device.find({ "user": user }, (err, devices) => {
+    return err
+    ? res.send(err)
+    : res.send(devices);
+    });
+});
+
+app.use(express.static(`${__dirname}/public`));
+
+/**
+* @api {get} /docs sends documentation
+* @apiGroup docs
+* @apiSuccessExample {html} Success-Response: 
+*index.html 
+*/
+
+
+app.get('/docs', (req, res) => {
+res.sendFile(`${__dirname}/public/generated-docs/index.html`);
+});
---
+++ web/public/app.js
@@ -0,0 +1,157 @@
+$('#navbar').load('navbar.html');
+$('#footbar').load('footer.html');
+
+const API_URL = 'https://trackme-one.vercel.app/api';
+
+const response = $.get(`${API_URL}/devices`);
+
+console.log(response);
+
+
+/*$.get(`${API_URL}/devices`).then(response => {response.forEach(device => {
+    $('#devices tbody').append(`
+    <tr>
+    <td>${device.user}</td>
+    <td>${device.name}</td>
+    </tr>`
+    );});
+})
+.catch(error => {
+console.error(`Error: ${error}`);
+});
+*/
+const currentUser = localStorage.getItem('user');
+if (currentUser) 
+{
+    $.get(`${API_URL}/users/${currentUser}/devices`).then(response => {
+        response.forEach((device) => {
+            console.log("'#devices tbody'");
+            $('#devices tbody').append(`
+            <tr data-device-id=${device._id}>
+            <td>${device.user}</td>
+            <td>${device.name}</td>
+            </tr>`
+            );
+        });console.log("'#devices tbody endeededed");
+        $('#devices tbody tr').on('click', (e) => {
+            console.log("#devices tbody trasdasdasdasdasdasqwd");
+            const deviceId = e.currentTarget.getAttribute('data-device-id');
+            $.get(`${API_URL}/devices/${deviceId}/device-history`).then(response => {response.map(sensorData => {
+                $('#historyContent').empty();
+                $('#historyContent').append(`
+                <tr>
+                <td>${sensorData.ts}</td>
+                <td>${sensorData.temp}</td>
+                <td>${sensorData.loc.lat}</td>
+                <td>${sensorData.loc.lon}</td>
+                </tr>
+                `);
+                });
+                $('#historyModal').modal('show');
+            });
+    });
+    })
+    .catch(error => {
+        console.error(`Error: ${error}`);
+    });
+}
+else
+{
+    const path = window.location.pathname;
+    if (path !== '/login')
+    {
+        //location.href = '/login';
+    }
+}
+
+const isAuthenticated=JSON.parse(localStorage.getItem('isAuthenticated')) || false;
+
+$('#add-device').on('click', () => {
+    const name = $('#name').val();
+    const user = $('#user').val();
+    const sensorData = [];
+    const body = {
+    name,
+    user,
+    sensorData
+    };
+    $.post(`${API_URL}/devices`, body)
+    .then(response => {
+    location.href = '/';
+    })
+    .catch(error => {
+    console.error(`Error: ${error}`);
+    });
+    });
+
+$('#register').on('click', function() {
+    const user = $('#user').val();
+    const password = $('#password').val();
+    const confirm = $('#confirm').val();
+    const isAdmin=false;
+
+    if(password!=confirm)
+    {
+        $(".message").empty();
+        $(".message").append("<p> Your Password and Confirm Password inputs do not match.</p>");
+        //location.href = '/registration';
+    }
+    else
+    {
+        $.post(`${API_URL}/registration`, { "name":user, "password":password, "isAdmin":isAdmin}).then((response) =>{if (response.success) {
+    //location.href = '/login';
+    console.log("registration successfull");
+    }
+    else {
+    $('#message').append(`<p class="alert alert-danger">${response}
+    </p>`);
+    }
+    });
+}
+
+
+});
+$('#login').on('click', () => {
+    const user = $('#username').val();
+    const password = $('#password').val();
+    //console.log("login click");
+    //console.log("user:"+user);
+    //console.log("password:"+password);
+    
+    //const _user={ "name":user, "password":password };
+    //console.log("sent"+_user);
+    $.post(`${API_URL}/authenticate`, { "name":user, "password":password })
+    .then((response) =>{
+        console.log("response");
+        console.log(response);
+    if (response.success) 
+    {
+        //console.log("response");
+        //console.log(response);
+        localStorage.setItem('user', user);
+        localStorage.setItem('isAdmin', response.isAdmin);
+        localStorage.setItem('isAuthenticated',true);
+        location.href = '/';
+    }
+    else
+    {
+        $('#message').append(`<p class="alert alert-danger">${response}</p>`);
+    }
+    });
+});
+
+
+
+const logout = () => {
+    localStorage.removeItem('user');
+    localStorage.removeItem('isAuthenticated');
+    location.href = '/login';
+    }
+
+
+
+$('#send-command').on('click', function() {
+    const command = $('#command').val();
+    console.log(`command is: ${command}`);
+});
+

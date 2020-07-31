9fe31766ed915150dfc421d6ba298bc620ccf474
deolsatish <deol.satish.2001.3@gmail.com>
2020-07-31 10:35:53 +1000

    send-command changes in app and added another field in send-command.html

--- web/public/app.js
+++ web/public/app.js
@@ -1,7 +1,7 @@
 $('#navbar').load('navbar.html');
 $('#footbar').load('footer.html');
 
-const API_URL = 'https://trackme-one.alpha.app/api';
+const API_URL = 'https://trackme-alpha.vercel.app/api';
 const MQTT_URL = 'http://localhost:5001';
 
 const response = $.get(`${API_URL}/devices`);
